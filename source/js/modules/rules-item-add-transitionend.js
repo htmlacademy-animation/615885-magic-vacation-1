eab427c7b11f74e63723391320f3c8a86e92cc71
Konstantin <sk7konstantin@yandex.ru>
2020-05-06 12:17:39 +0300

    Исправил появление кнопки и замечания editorconfig

--- source/js/modules/rules-item-add-transitionend.js
+++ source/js/modules/rules-item-add-transitionend.js
@@ -1,9 +1,13 @@
 export default ()=> {
-  let lastItemRules = document.querySelector('.rules__list li:last-child p');
-  let linkRulesBefore = document.querySelector('.rules__link');
+  let lastItemRules = document.querySelector(`.rules__list li:last-child p`);
+  let linkRules = document.querySelector(`.rules__link`);
+  let linkRulesText = document.querySelector(`.rules__link span`);
 
-  lastItemRules.addEventListener('animationend', function(evt) {
-    linkRulesBefore.style.transformOrigin = '100% 50%';
-    linkRulesBefore.style.animation = 'show-btn 0.65s ease-out forwards';
-  }, false)
-}
+  lastItemRules.addEventListener(`animationend`, function () {
+    linkRules.classList.add(`rules__link--bg-before`);
+    linkRulesText.classList.add(`rules__link__text--show`);
+    linkRules.style.opacity = `1`;
+    linkRules.style.transformOrigin = `100% 50%`;
+    // linkRules.style.animation = 'show-btn 0.45s ease-in-out forwards';
+  }, false);
+};
