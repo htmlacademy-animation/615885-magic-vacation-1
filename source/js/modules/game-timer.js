861c8a5557feaf7f6e30afe0d7e2fa580c615861
Konstantin <sk7konstantin@yandex.ru>
2020-06-06 11:26:58 +0300

    Обернул вызов таймера в setInterval

--- source/js/modules/game-timer.js
+++ source/js/modules/game-timer.js
@@ -1,31 +1,40 @@
 export default ()=> {
+  let linkGame = document.querySelector(`a[href="#game"]`);
+  let screenGame = document.querySelector(`.screen--game`);
   let minutesSpan = document.querySelector(`.game__minutes`);
   let secondsSpan = document.querySelector(`.game__seconds`);
+  let fpsInterval = 1000 / 11;
+  let intervalTimer;
+  let countDownDate;
 
-  function wrapperForTimer(SS) {
+  function timer() {
+    let now = Date.now();
+    let distance = countDownDate - now;
 
-    let countDownDate = new Date().getTime() + 1000 * SS;
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    function timer() {
-      let now = Date.now();
-      let distance = countDownDate - now;
-
-      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      if (distance > 0) {
-        requestAnimationFrame(timer);
-      } else {
-        return;
-      }
-
-      minutesSpan.innerHTML = minutes < 10 ? `0` + minutes : minutes;
-      secondsSpan.innerHTML = seconds < 10 ? `0` + seconds : seconds;
+    if (distance > 0) {
+      requestAnimationFrame(timer);
+    } else {
+      clearInterval(intervalTimer);
+      return;
     }
 
-    requestAnimationFrame(timer);
-
+    minutesSpan.innerHTML = minutes < 10 ? `0` + minutes : minutes;
+    secondsSpan.innerHTML = seconds < 10 ? `0` + seconds : seconds;
   }
 
-  wrapperForTimer(300);
+  function startTimer(min) {
+    countDownDate = new Date().getTime() + 1000 * min;
+    intervalTimer = setInterval(timer, fpsInterval);
+  }
+
+  if (screenGame.classList.contains(`active`)) {
+    startTimer(300);
+  }
+
+  linkGame.addEventListener(`click`, function () {
+    startTimer(300);
+  });
 };
